94384681a2ab3953f990a956597d384cd59da3a2
Asapanna Rakesh <rakeshark22@gmail.com>
2024-03-26 05:19:23 +0530

    include build

---
+++ streamlit_image_select/frontend/build/precache-manifest.a3ed1e0789f7e4b715083db6f7d3fcad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fef722a5012cd02cd1c8dc8836c1c7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9a1e852f972414fe4f0",
+    "url": "./static/js/2.761ae6da.chunk.js"
+  },
+  {
+    "revision": "b5321db7731dbb9a09b1fc4c60b61213",
+    "url": "./static/js/2.761ae6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "405aa8fa5573cfcf836e",
+    "url": "./static/js/main.f9f36653.chunk.js"
+  },
+  {
+    "revision": "08bfe9a8f9db97a558b1",
+    "url": "./static/js/runtime-main.fce4c018.js"
+  }
+]);
